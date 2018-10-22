a9725e4806719d76cd133e8e2e82482a1d8dd5aa
Corné <cornehaasjes@gmail.com>
2018-10-22 11:57:46 +0200

    add segment class for more structured code

--- photon/src/photon_threads.ino
+++ photon/src/photon_threads.ino
@@ -1,167 +1,133 @@
 #include "MQTT.h"
 
+// CONSTANTS (CONFIGURATION)
+double dx = 0.5;                   // distance between sensors in meter
+int minTimeBetweenInterrupts = 60; // minimum time between two interrupts
+
 MQTT client("145.94.196.251", 1883, callback);
 
-Thread thread("mqttThread", MQTTSend);
+//Thread thread("mqttThread", MQTTSend);
 
 // recieve message
 void callback(char *topic, byte *payload, unsigned int length)
 {
 }
 
-boolean hasFrontWheelPassedOne = false;
-boolean hasFrontWheelPassedBoth = false; 
-boolean hasRearWheelPassedOne = false;
-int firstSensor;
+// Define Segment class
+#pragma region
 
-unsigned long frontWheelTime = 0;
-unsigned long rearWheelTime = 0;
-unsigned long lastInterruptTime0 = 0;
-unsigned long lastInterruptTime1 = 0;
+class Segment
+{
+  public:
+    int segmentId;
+    long timestamp;
+    long triggerTimes[4];
 
-boolean isDoubleInterrupt0 = false;
-boolean isDoubleInterrupt1 = false;
+    Segment(int id);
 
-String dataToSend = "";
+    void getTriggerTimeA();
+    void getTriggerTimeB();
+    double calculateSpeed();
+    void reset();
 
-double frontWheelVelocity = 0.0;
+  private:
+    long lastTriggerTimeA;
+    long lastTriggerTimeB;
+};
 
-int i = 0;
+Segment::Segment(int id)
+{
+    segmentId = id;
+}
 
-double dx = 0.025; // distance between both sensors
+void Segment::getTriggerTimeA()
+{
+    long triggerTime = millis();
+    if (triggerTime - lastTriggerTimeA > minTimeBetweenInterrupts)
+    {
+        if (triggerTimes[0] == 0)
+        {
+            triggerTimes[0] = triggerTime;
+        }
+        else if (triggerTimes[1] == 0)
+        {
+            triggerTimes[1] = triggerTime;
+        }
+    }
+
+    lastTriggerTimeA = triggerTime;
+}
+
+void Segment::getTriggerTimeB()
+{
+    long triggerTime = millis();
+    if (triggerTime - lastTriggerTimeB > minTimeBetweenInterrupts)
+    {
+        if (triggerTimes[2] == 0)
+        {
+            triggerTimes[2] = triggerTime;
+        }
+        else if (triggerTimes[3] == 0)
+        {
+            triggerTimes[3] = triggerTime;
+        }
+    }
+
+    lastTriggerTimeB = triggerTime;
+}
+
+double Segment::calculateSpeed()
+{
+    double speed1 = dx / (triggerTimes[2] - triggerTimes[0]);
+    double speed2 = dx / (triggerTimes[3] - triggerTimes[1]);
+    return (speed1 + speed2) / 2;
+}
+
+void Segment::reset()
+{
+    timestamp = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        triggerTimes[i] = 0;
+    }
+}
+
+#pragma endregion Segment class definition
+
+// Declare Segment class instances
+#pragma region
+
+Segment segment1 = Segment(1);
+
+void getTriggerTimeSegment1A()
+{
+    segment1.getTriggerTimeA();
+}
+
+void getTriggerTimeSegment1B()
+{
+    segment1.getTriggerTimeB();
+}
+
+#pragma endregion Declare Segment Class Instances
 
 void setup()
 {
-    Serial.begin(9600);
+    // Segment 1
     pinMode(D1, INPUT_PULLUP);
+    attachInterrupt(D1, getTriggerTimeSegment1A, RISING);
+
     pinMode(D2, INPUT_PULLUP);
+    attachInterrupt(D2, getTriggerTimeSegment1B, RISING);
 
-    attachInterrupt(D1, velocityMeasure0, RISING);
-    attachInterrupt(D2, velocityMeasure1, RISING);
+    //Segment 2
 
-    client.connect("photon");
-    client.subscribe("/test");
-    client.publish("/test", "hoi");
+    //Segment 3
+
+    //Segment 4
 }
 
-void loop()
-{
+void loop() {
 
-    if (millis() - frontWheelTime > 3500 && hasFrontWheelPassedOne)
-    { // een meting die langer duurt dan 3 seconden wordt afgebroken.
-        resetR();
-        Serial.println("expire \n");
-    }
-    delay(100);
-}
-
-void MQTTSend()
-{ // function for multithreading
-    while (true)
-    {
-        if (client.isConnected())
-        {
-            client.loop();
-        }
-        else
-        {
-            client.connect("photon");
-        }
-        client.publish("/test", dataToSend);
-    }
-}
-
-void interrupt()
-{
-    if (millis() - lastInterruptTime0 > 50)
-    {
-        Serial.println("Triggered");
-        lastInterruptTime0 = millis();
-    }
-}
-
-void velocityMeasure0()
-{
-
-    if (millis() - lastInterruptTime0 > 50)
-    {
-        velocityMeasure(0);
-        lastInterruptTime0 = millis();
-    }
-}
-
-void velocityMeasure1()
-{
-    if (millis() - lastInterruptTime1 > 50)
-    {
-        velocityMeasure(1);
-        lastInterruptTime0 = millis();
-    }
-}
-
-void velocityMeasure(int sensor)
-{
-    if (!hasFrontWheelPassedOne)
-    { // first detection of bike, measurement starts
-        frontWheelTime = millis();
-        firstSensor = sensor;
-        hasFrontWheelPassedOne = true;
-    }
-    else if (sensor == firstSensor)
-    { // rear wheel detected for the first time
-        hasRearWheelPassedOne = true;
-        rearWheelTime = millis();
-    }
-    else
-    {
-        int dt;
-        if (hasFrontWheelPassedBoth)
-        { // calculate rear wheel speed
-            unsigned long rearWheelTimeTwo = millis();
-            dt = rearWheelTimeTwo - rearWheelTime;
-            double rearWheelVelocity = 1000 * dx / dt;
-            Serial.println(rearWheelTime);
-            Serial.println(rearWheelTimeTwo);
-            sendVelocity((frontWheelVelocity + rearWheelVelocity) / 2, 1, firstSensor); // send average of front and rear wheel speed
-            Serial.println();
-            resetR(); // stop measurement
-        }
-
-        else
-        { // calculate front wheel speed
-            hasFrontWheelPassedBoth = true;
-            unsigned long frontWheelTimeTwo = millis();
-            Serial.println(frontWheelTime);
-            Serial.println(frontWheelTimeTwo);
-            dt = frontWheelTimeTwo - frontWheelTime;
-            frontWheelVelocity = 1000 * dx / dt;
-            //Serial.println(frontWheelVelocity);
-        }
-    }
-}
-
-void sendVelocity(double velocity, int segment, int direction)
-{
-    Serial.println(velocity);
-    if(direction == 0){
-        dataToSend = "{ \"velocity\": " + String(velocity) + ", \"segment\": " + String(segment) + "}";
-    }
-    else if(direction == 1){
-        dataToSend = "{ \"velocity\": -" + String(velocity) + ", \"segment\": " + String(segment) + "}";
-    }
-}
-
-void resetR()
-{
-    hasFrontWheelPassedOne = false;
-    hasFrontWheelPassedBoth = false;
-    hasRearWheelPassedOne = false;
-    isDoubleInterrupt0 = false;
-    isDoubleInterrupt1 = false;
-    firstSensor = 0;
-
-    frontWheelTime = 0;
-    rearWheelTime = 0;
-
-    frontWheelVelocity = 0.0;
 }
