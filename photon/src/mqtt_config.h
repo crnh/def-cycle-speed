31caea83635fa9cce70c0fe2a90f57d8fe9e2fd0
JonathanDingemanse <jonathandingemanse@kliksafe.nl>
2018-10-26 13:31:41 +0200

    geen idee

---
+++ photon/src/mqtt_config.h
@@ -0,0 +1,8 @@
+// MQTT definitions that shouldn't be exposed via git
+
+#define MQTT_HOST "defmqtt.ddns.net"
+#define MQTT_PORT 1883
+
+#define MQTT_CLIENT_ID "photon"
+#define MQTT_USER "fietsenmoordenaar"
+#define MQTT_PASS "FhW2sCcqxyoU5XiwYLPgfpNoz8y9lQ3gGfL8zREJz5qs9pGeQlCvRw6EySAeInw8"
