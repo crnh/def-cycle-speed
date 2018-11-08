bdf1be381be3c6ec87be26553e90c4ccf6d65498
Corné <cornehaasjes@gmail.com>
2018-11-08 18:57:21 +0100

    add comments

--- electron/src/electron.ino
+++ electron/src/electron.ino
@@ -1,15 +1,17 @@
 SYSTEM_THREAD(ENABLED);
 
-const char datatopic[] = "bike/data";
+const char datatopic[] = "bike/data";           // topic for publishing datasets
 const char statustopic[] = "bike/status";
+const int dataBufferSize = 128;                 // size of data buffer array used to queue messages
 
-const int timeout = 1000;                       // maximum measurement time in microseconds
-const long connectTime = 180000;                //time allowed to get connection before timing out
+const int maximumMeasurementDuration = 1000;    // maximum measurement time in microseconds
+const long connectTime = 180000;                // time allowed to get connection before timing out
 const int minimumTimeBetweenPublishes = 1100;   // minimum time between two publishes to the Particle Cloud
-const long noConnectionSleepTime = 300000;      //wait 5 minutes before re-connecting if no connection.
+const long noConnectionSleepTime = 300000;      // wait 5 minutes before re-connecting if no connection.
 const double dx = 1.0;                          // distance between both sensors in meters
 const unsigned long sleepTimeout = 100;         // time to stay awake after measurement in seconds.
 
+// Start data publish thread
 Thread thread("publishThread", publishToCloud);
 
 int firstSensor[] = {0, 0, 0, 0};
@@ -32,9 +34,7 @@ void setup()
     Serial.begin(115200);
     Serial.println("test");
     
-    //RGB.control(true);
-    //RGB.color(0, 0, 0);
-
+    // Set pinmodes for interrupts
     pinMode(D2, INPUT_PULLUP);
     pinMode(A2, INPUT_PULLUP);
     pinMode(D1, INPUT_PULLUP);
@@ -44,6 +44,7 @@ void setup()
     pinMode(B0, INPUT_PULLUP);
     pinMode(D3, INPUT_PULLUP);
 
+    // Define interrupt pins
     attachInterrupt(D2, velocityMeasure0A, FALLING);
     attachInterrupt(A2, velocityMeasure0B, FALLING);
     attachInterrupt(D1, velocityMeasure1A, FALLING);
@@ -53,7 +54,8 @@ void setup()
     attachInterrupt(B0, velocityMeasure3A, FALLING);
     attachInterrupt(D3, velocityMeasure3B, FALLING);
 
-    for (int i = 0; i < 128; i++)
+    // Initialize data buffer (Particle Cloud allows only 1 message per second)
+    for (int i = 0; i < dataBufferSize; i++)
     {
         dataToSendArray[i] = "";
     }
@@ -65,12 +67,15 @@ void loop()
 {
     for (int j = 0; j < 4; j++)
     {
-        if (millis() - frontWheelTime[j] > 3000 && frontWheelTime[j] != 0)
-        { // een meting die langer duurt dan 3 seconden wordt afgebroken.
+        // Reset if a measurement takes longer than the maximum measurement duration
+        if (millis() - frontWheelTime[j] > maximumMeasurementDuration && frontWheelTime[j] != 0)
+        {
             resetSegment(j);
             Serial.println("expire \n");
         }
     }
+
+    // Turn of cellular if there are no measurements during sleepTimeout
     if (Time.now() - lastMeasurementTime > sleepTimeout && dataToSendArray[0].equals("") && Cellular.ready())
     {
         Serial.println("sleep");
@@ -78,10 +83,12 @@ void loop()
         delay(1000);
         Cellular.off();
     }
-    //Serial.println(".");
+
     delay(10);
 }
 
+
+// Define interrupt methods
 void velocityMeasure0A()
 {
     debounceAndMeasure(0, 0);
@@ -122,10 +129,13 @@ void velocityMeasure3B()
     debounceAndMeasure(3, 1);
 }
 
+// Function that is called when an interrupt is fired
 void debounceAndMeasure(int segment, int sensor)
 {
     Serial.println("triggered");
     int index = 2 * segment + sensor;
+
+    // Software debounce and register interrupt time
     if (millis() - lastInterruptTime[index] > 75)
     {
         Serial.println("triggered " + String(segment) + "," + String(sensor));
@@ -134,6 +144,7 @@ void debounceAndMeasure(int segment, int sensor)
     }
 }
 
+// Process triggers to measure velocity
 void velocityMeasure(int i, int sensor)
 {
     if (frontWheelTime[i] == 0)
@@ -178,12 +189,13 @@ void velocityMeasure(int i, int sensor)
     }
 }
 
+// Add measured velocity to data buffer
 void sendVelocity(unsigned long timestamp, double velocity, int segment, int direction)
 {
-    //Serial.println(velocity);
     int velocitySign = direction * 2 - 1; // determine the sign of the velocity
     dataToSend = String(timestamp) + "," + String(velocitySign * velocity) + "," + String(segment);
 
+    // Add data to data buffer
     for (int i = 0; i < 128; i++)
     {
         if (dataToSendArray[i].equals(""))
@@ -193,22 +205,18 @@ void sendVelocity(unsigned long timestamp, double velocity, int segment, int dir
         }
     }
 
-    /*
-    if (direction == 0){}
-    else if (direction == 1){
-        dataToSend = "{\"vel\": -" + String(velocity) + ", \"seg\": " + String(segment) + ", \"time\": " + String(timeStamp) + " }";
-    }*/
     Serial.println(dataToSend);
 }
 
+// Check for data in data buffer and publish it to the cloud
 void publishToCloud()
 {
-    int length = 128;
     String stringToSend;
     int amountToSend;
     while (true)
     {
-        if(!Cellular.ready() && !dataToSendArray[4].equals("")){ // The connection will be restarted if 5 or more bikes are detected.
+        // If there is no connection, the connection will be restarted if 5 or more bikes are detected.
+        if(!Cellular.ready() && !dataToSendArray[4].equals("")){ 
             Serial.println("trying to connect");
             Cellular.connect();
             unsigned long startConnecting = millis();
@@ -220,8 +228,8 @@ void publishToCloud()
                 Serial.println("sending data");
                 stringToSend = "";
                 amountToSend = 0;
-                for (int i = 0; i < length; i++)
-                { // Up till 9 strings from dataToSendArray are combined in stringToSend, seperated by a comma
+                for (int i = 0; i < dataBufferSize; i++)
+                { // Up to 9 strings from dataToSendArray are combined in stringToSend, seperated by a comma
                     if (dataToSendArray[i].equals("") || amountToSend >= 9)
                     {
                         break;
@@ -237,11 +245,11 @@ void publishToCloud()
                         stringToSend += ";";
                     }
                 }
-                for (int i = 0; i < length - amountToSend; i++)
+                for (int i = 0; i < dataBufferSize - amountToSend; i++)
                 { // dataToSendArray elements are shifed to the left
                     dataToSendArray[i] = dataToSendArray[i + amountToSend];
                 }
-                for (int i = length - amountToSend; i < length; i++)
+                for (int i = dataBufferSize - amountToSend; i < dataBufferSize; i++)
                 { // last places will be cleared
                     dataToSendArray[i] = "";
                 }
@@ -260,14 +268,12 @@ void publishToCloud()
     }
 }
 
+// Reset a segment, called when a measurement expires or finishes
 void resetSegment(int i)
 {
     timestamp[i] = 0;
-
     firstSensor[i] = 0;
-
     frontWheelTime[i] = 0;
     rearWheelTime[i] = 0;
-
     frontWheelVelocity[i] = 0.0;
 }
